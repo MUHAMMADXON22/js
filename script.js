ae348ed601b5749bcf2bdc17125ba2fad256b00f
MUKHAM <muxammadxon994@gmail.com>
2025-03-11 10:09:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("name");
+//alert("oshibka");
+
+var box = ''; //старый
+console.log(box);
+
+let age = 196 ; //сейчас используют
+console.log(age);
+
+const text = 'Miya yoq'; //
+console.log(text);
+
+let name = prompt("Введите ваше имя");
+alert(`привет ${name}`)
+
+let vozvrast = prompt ("Укажите свой возвраст");
+alert(` Меня зовут ${name}, мне ${vozvrast} лет` );
+
+let manzil = prompt ("Укажите свой адрес");
+alert(` Мой фдрес ${manzil}, живу ${manzil} ` );
